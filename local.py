db9081601fdd8b70facd9d92df0397bc24a91680
duveen <duveen@duveen.me>
2021-03-28 01:02:03 +0900

    DEEPEC Preprocess Code Change

--- local.py
+++ local.py
@@ -1,4 +1,5 @@
 from app import create_app
+from app import preprocess
 from config.app_config import LocalLevelConfig
 from config.db_config import LocalConfig
 from config.run_config import LocalRunConfig
@@ -6,4 +7,6 @@ from config.run_config import LocalRunConfig
 app = create_app(LocalLevelConfig, LocalConfig)
 
 if __name__ == "__main__":
+    preprocess.preprocess_deepec()
+
     app.run(**LocalRunConfig)
